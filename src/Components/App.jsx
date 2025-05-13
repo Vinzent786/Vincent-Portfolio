cf33a30fdef1acd68b50cee2db3335279166e09f
Vincent <vinzent0404@gmail.com>
2025-05-13 02:25:51 -0400

    Added exp images into image preloader; switched pngs to webps

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -52,10 +52,13 @@ const Skills = lazy(() => import("./AppLayout/Skills.jsx"));
 // - Wraps routes with the theme wrapper (used for components outside of main path),
 // - Wraps routes in the suspense provider (to render fallback UI while lazy loading components render)
 export default function App() {
-  // Preloads the background image for home route and theme switcher
+  // Preloads the background image for home route, theme switcher, and exp component
   usePreloadImages([
     '/assets/code-cover.webp',
-    '/assets/DayNightIcon.svg'
+    '/assets/DayNightIcon.svg',
+    '/assets/front-end-dev.webp',
+    '/assets/back-end-dev.webp',
+    '/assets/system-admin.webp'
    ]);
 
   return (
